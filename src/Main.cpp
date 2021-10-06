8aa440bca2689acad2bf3af98ceee1a912a374db
Martin <martbelko@gmail.com>
2021-10-06 22:46:43 +0200

    Testing GLFW and Glad

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,36 +1,54 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main(void)
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main()
 {
-	GLFWwindow* window;
+	glfwInit();
 
-	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
-
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
+	GLFWwindow* window = glfwCreateWindow(800, 600, "PBR App", NULL, NULL);
+	if (window == NULL)
 	{
+		std::cout << "Failed to create GLFW window\n";
 		glfwTerminate();
 		return -1;
 	}
 
-	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	/* Loop until the user closes the window */
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD\n";
+		return -1;
+	}
+
 	while (!glfwWindowShouldClose(window))
 	{
-		/* Render here */
+		processInput(window);
 
-		/* Swap front and back buffers */
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
 		glfwPollEvents();
 	}
 
 	glfwTerminate();
 	return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
 }
