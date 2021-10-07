b15050fa23acc3035dacef0c3fdc95be4aef815f
Martin <martbelko@gmail.com>
2021-10-07 02:27:19 +0200

    Added KeyEvent, KeyPressedEvent, KeyReleasedEvent and KeyTypedEvent classes

---
+++ src/Events/KeyEvent.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <sstream>
+
+#include "Events/Event.h"
+#include "KeyCodes.h"
+
+class KeyEvent : public Event
+{
+public:
+	KeyCode getKeyCode() const { return mKeyCode; }
+
+	EVENT_CLASS_CATEGORY(static_cast<int>(Category::Keyboard) | static_cast<int>(Category::Input))
+protected:
+	KeyEvent(const KeyCode keycode)
+		: mKeyCode(keycode) {}
+
+	KeyCode mKeyCode;
+};
+
+class KeyPressedEvent : public KeyEvent
+{
+public:
+	KeyPressedEvent(const KeyCode keycode, const uint16_t repeatCount)
+		: KeyEvent(keycode), mRepeatCount(repeatCount) {}
+
+	uint16_t getRepeatCount() const { return mRepeatCount; }
+
+	std::string toString() const override
+	{
+		std::stringstream ss;
+		ss << "KeyPressedEvent: " << static_cast<int>(mKeyCode) << " (" << mRepeatCount << " repeats)";
+		return ss.str();
+	}
+
+	EVENT_CLASS_TYPE(Type::KeyPressed)
+private:
+	uint16_t mRepeatCount;
+};
+
+class KeyReleasedEvent : public KeyEvent
+{
+public:
+	KeyReleasedEvent(const KeyCode keycode)
+		: KeyEvent(keycode) {}
+
+	std::string toString() const override
+	{
+		std::stringstream ss;
+		ss << "KeyReleasedEvent: " << static_cast<int>(mKeyCode);
+		return ss.str();
+	}
+
+	EVENT_CLASS_TYPE(Type::KeyReleased)
+};
+
+class KeyTypedEvent : public KeyEvent
+{
+public:
+	KeyTypedEvent(const KeyCode keycode)
+		: KeyEvent(keycode) {}
+
+	std::string toString() const override
+	{
+		std::stringstream ss;
+		ss << "KeyTypedEvent: " << static_cast<int>(mKeyCode);
+		return ss.str();
+	}
+
+	EVENT_CLASS_TYPE(Type::KeyTyped)
+};
